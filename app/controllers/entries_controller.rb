bbaebebb38e6829fba1c99f1e15bc54609bb461a
jeff-gorski <jeff.gorski@kellogg.northwestern.edu>
2026-02-24 02:47:46 +0000

    establish routes, controllers, & views

---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,23 @@
+class EntriesController < ApplicationController
+
+  def new
+    # find the Place this entry belongs to
+    @place = Place.find_by({ "id" => params["place_id"] })
+    # render view with new Entry form
+  end
+
+  def create
+    # start with a new Entry
+    @entry = Entry.new
+    # assign user-entered form data to Entry's columns
+    @entry["title"] = params["title"]
+    @entry["description"] = params["description"]
+    @entry["posted_on"] = params["posted_on"]
+    @entry["place_id"] = params["place_id"]
+    # save Entry row
+    @entry.save
+    # redirect user
+    redirect_to "/places/#{@entry["place_id"]}"
+  end
+
+end
